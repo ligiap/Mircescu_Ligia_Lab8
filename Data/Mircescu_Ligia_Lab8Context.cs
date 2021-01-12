34ce5dfade35073ea2c6c4eaa657254b2d8d97ae
Ligia Mangra <Ligia.Mangra@Yardi.Com>
2021-01-12 10:54:43 +0200

    Add project files.

---
+++ Data/Mircescu_Ligia_Lab8Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Mircescu_Ligia_Lab8.Models;
+
+namespace Mircescu_Ligia_Lab8.Data
+{
+    public class Mircescu_Ligia_Lab8Context : DbContext
+    {
+        public Mircescu_Ligia_Lab8Context (DbContextOptions<Mircescu_Ligia_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Mircescu_Ligia_Lab8.Models.Book> Book { get; set; }
+    }
+}
